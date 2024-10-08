c26ff1dc37ea0d44630591426007eb6a8368a2fb
Diego <fdiegofreire@gmail.com>
2024-10-07 21:57:22 -0300

    adicionando passo a automação

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import pyautogui as bot
-bot.PAUSE=1
+bot.PAUSE=1.5
 def abrir_login(): #função para abrir o login do site da prefeitura
     bot.hotkey('alt', 'tab')
     cidadao = bot.locateCenterOnScreen('botoes/cidadao.png')
@@ -7,10 +7,10 @@ def abrir_login(): #função para abrir o login do site da prefeitura
     y_offset=50
     bot.moveTo(cidadao.x, cidadao.y+y_offset)                               
     bot.click()
-    bot.sleep(5)
     bot.click(bot.locateCenterOnScreen('botoes/nfs.png'))
     bot.click(bot.locateCenterOnScreen('botoes/nfs2.png'))
     bot.click(x=989, y=922)
+
 def get_field_positions(): #função que pega as posições dos campos CNPJ, CPF e SENHA
     locate_cnpj=bot.locateCenterOnScreen('botoes/cnpj.png')
     locate_cpf=bot.locateCenterOnScreen('botoes/cpf.png')
@@ -27,17 +27,13 @@ def click_field(position):
     bot.click(position)
 
 def fill_login(cnpj, cpf, senha):
-    bot.sleep(1)
     positions=get_field_positions()   #pegando o dicionário de posições
     click_field(positions['cnpj'])  #clicando na posição do cnpj pela da chave 'cnpj' do dicionário que retorna a posiçao do campo
     bot.write(cnpj)
-    bot.click(x=1483, y=429)
-    click_field(positions['cpf'])
+    click_field(positions['cpf'])#clicando na posição do cnpj pela da chave 'cpf' do dicionário que retorna a posiçao do campo do npj
     bot.write(cpf)
-    bot.click(x=1483, y=429)   #clicando na posição do cnpj pela da chave 'cpf' do dicionário que retorna a posiçao do campo do npj
-    click_field(positions['senha'])
-    bot.write(senha)
-    bot.click(x=1483, y=429) #clicando na posição do cnpj pela da chave 'senha' do dicionário que retorna a posiçao do campo
+    click_field(positions['senha'])#clicando na posição do cnpj pela da chave 'senha' do dicionário que retorna a posiçao do campo
+    bot.write(senha) 
     click_field(positions['entrar']) #clicando no botão de entrar
 
 def get_infos(arquivo):
@@ -51,15 +47,19 @@ def get_infos(arquivo):
     return credenciais
 
 def get_relatorios():
-    bot.click(bot.locateCenterOnScreen())
-    bot.click(' ')
+    bot.click(bot.locateCenterOnScreen('botoes/relatorio.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/date.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/back.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/day1.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/date.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/back.png'))
+    bot.click(bot.locateCenterOnScreen('botoes/day30.png'))
 
-abrir_login()
 bot.sleep(1)
-credenciais = get_infos('teste-db.txt')
-for cnpj, cpf, senha in credenciais:
-    fill_login(cnpj, cpf, senha)
-    bot.sleep(8)
+#credenciais = get_infos('teste-db.txt')#falta fazer a parte de abrir o txt, pegar as informações de cnpj, colar no cnpj, depois cpf e senha e assim, fazer o login de cada empresa
+#for cnpj, cpf, senha in credenciais:
+    #fill_login(cnpj, cpf, senha)
+    #bot.sleep(5)
+get_relatorios()
 
-#falta fazer a parte de abrir o txt, pegar as informações de cnpj, colar no cnpj, depois cpf e senha e assim, fazer o login de cada empresa
 
